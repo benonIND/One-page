0f30a2ca5bfc7d0a317ab83e3efaa49446d48e2a
benonIND <56250907+benonIND@users.noreply.github.com>
2020-11-14 15:28:40 +0700

    Update one-page.php

--- one-page.php
+++ one-page.php
@@ -35,7 +35,207 @@
         - Note!     : Template ini saya buat untuk di gunakan secara FREE!!,
                       Tidak ada yang di jual belikan,
                       Jika ada yang menjual,belikan template ini,
+                      Mengatas namakan saya atau dya sendiri semoga mendapat dosa besar Aamiin :).<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Primary page deepnet7">
+        <meta name="robots" content="index, follow">
+        <meta name="googlebot" content="index, follow">
+        <meta name="keywords" content="deepnet, deepnet7, deepnet7.com, benon, BENON_">
+        <meta name="author" content="BENON_">
+        <meta property="og:type" content="website">
+        <link href="https://deepnet7.com/assets/img/bg.jpg" rel="icon">
+        <meta property="og:site_name" content="deepnet7.com">
+        <meta property="og:image" content="https://deepnet7.com/assets/img/bg.jpg">
+        <meta property="og:description" content="primary page deepnet7">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <style>
+            .op-color{
+                background: linear-gradient(to bottom right, #00FFFF,#F0FFFF);
+            }
+        </style>
+        <title> official deepnet7</title>
+    </head>
+    <body>
+    <!--
+        - Build by  : BENON_
+        - type      : One Page template
+        - Component : Bootstrap 4,
+                      Font awesome,
+                      And with my logic
+        - Source    : Open
+        - Note!     : Template ini saya buat untuk di gunakan secara FREE!!,
+                      Tidak ada yang di jual belikan,
+                      Jika ada yang menjual,belikan template ini,
                       Mengatas namakan saya atau dya sendiri semoga mendapat dosa besar Aamiin :).
+        - Group pblc: https://chat.whatsapp.com/IfCnCABeSQq2K9wIbg7UNu
+    -->
+        <div class="container-fluid mb-3">
+            <nav class="row navbar sticky-top navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="/"> deepnet7 </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="https://movies.deepnet7.com"> nonton film </a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="https://blog.deepnet7.com"> blog </a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              Sub menu
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="#">Opsional 1</a>
+                                <a class="dropdown-item" href="#">Opsional 2</a>
+                                <a class="dropdown-item" href="#">Opsional 3</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+    
+            <!-- header -->
+            <div class="row op-color d-block px-3 pt-3 text-center">
+                <h1> Official Deepnet7 </h1>
+                <div class="p-3 mx-3 font-italic text-muted">
+                    "Menggapaimu sama halnya melakukan debbuging, 
+                    sulit tapi pasti,
+                    Response mu seperti protokol http yang belum SSD drive, lambat :("
+                </div>
+                <div class="pb-3">
+                    <button class="btn btn-sm btn-primary rounded-pill">
+                        Learn more
+                    </button>
+                </div>
+            </div>
+            <!-- header -->
+            
+            <!-- main content -->
+            <div class="row my-5 text-center">
+                <div class="col-3 p-3">
+                    <i class="fas fa-user-lock fa-4x"></i>
+                    <div class="p-3 text-muted">
+                        <footer class="blockquote-footer">
+                            height secure
+                        </footer>
+                    </div>
+                </div>
+                <div class="col-3 p-3">
+                    <i class="fas fa-user-graduate fa-4x"></i>
+                    <div class="p-3 text-muted">
+                        <footer class="blockquote-footer">
+                            Terlatih
+                        </footer>
+                    </div>
+                </div>
+                <div class="col-3 p-3">
+                    <i class="fas fa-server fa-4x"></i>
+                    <div class="p-3 text-muted">
+                        <footer class="blockquote-footer">
+                            server cepat
+                        </footer>
+                    </div>
+                </div>
+                <div class="col-3 p-3">
+                    <i class="fas fa-users-cog fa-4x"></i>
+                    <div class="p-3 text-muted">
+                        <footer class="blockquote-footer">
+                            selalu update system
+                        </footer>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="border bg-light text-center p-3 shadow-sm rounded-top">
+                <img src="https://deepnet7.com/images/benon.jpg" width="100px" height="100px" class="rounded-circle">
+                <div class="mt-3 text-center">
+                    <footer class="blockquote-footer font-italic">
+                        cerdas tidak butuh belajar, 
+                        hanya butuh ingatan tajam, pengalaman, dan logika.
+                    </footer>
+                </div>
+            </div>
+            <!-- Footer -->
+            <footer class="bg-info">
+            
+              <!-- Footer Elements -->
+              <div class="container">
+            
+                <!-- Grid row-->
+                <div class="row d-block">
+            
+                  <!-- Grid column -->
+                  <div class="col-md-12 py-5">
+                    <div class="text-center">
+            
+                      <!-- Facebook -->
+                      <a class="fb-ic">
+                        <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                      </a>
+                      <!-- Twitter -->
+                      <a class="tw-ic">
+                        <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                      </a>
+                      <!-- Google +-->
+                      <a class="gplus-ic">
+                        <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                      </a>
+                      <!--Linkedin -->
+                      <a class="li-ic">
+                        <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                      </a>
+                      <!--Instagram-->
+                      <a class="ins-ic">
+                        <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                      </a>
+                      <!--Pinterest-->
+                      <a class="pin-ic">
+                        <i class="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+                      </a>
+                    </div>
+                  </div>
+                  <!-- Grid column -->
+            
+                </div>
+                <!-- Grid row-->
+            
+              </div>
+              <!-- Footer Elements -->
+            
+              <!-- Copyright -->
+              <div class="bg-light border text-center py-3">© 2020 Copyright:
+                <a href="https://deepne7.com/"> deepnet7.com </a>
+              </div>
+              <!-- Copyright -->
+            
+            </footer>
+            <!-- Footer -->
+        </div>
+        <!-- Optional JavaScript; choose one of the two! -->
+    
+        <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    
+        <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+        -->
+    </body>
+</html>
         - Group pblc: https://chat.whatsapp.com/IfCnCABeSQq2K9wIbg7UNu
     -->
         <div class="container-fluid mb-3">
